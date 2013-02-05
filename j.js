f6c97d46911e4d465bc8b67cd8872ebbf42a8779
Anders Bakken <agbakken@gmail.com>
2013-02-05 00:55:07 -0800

    Syntax example

--- j.js
+++ j.js
@@ -1,10 +1,18 @@
 // j.js
 
 
-var target = {
-    name:"test",
-    sources: ['main.cpp', 'foo.cpp', {source:test.cpp, CXXFLAGS:"-g $CXXFLAGS"}, {source:utenstandardflags.cpp, CXXFLAGS:"-O3"} ],
-    CXXFLAGS: "-O2",
-    type:"library"
-}
+// output:
+// var target = {
+//     name:"test",
+//     SOURCES: ['main.cpp', 'foo.cpp', {source:test.cpp, CXXFLAGS:"-g $CXXFLAGS"}, {source:utenstandardflags.cpp, CXXFLAGS:"-O3"} ],
+//     CXXFLAGS: "-O2",
+//     type:"library"
+// }
+// syntax:
+
+SOURCES += "main.cpp foo.cpp";
+CXXFLAGS = "-02";
+TYPE = "library";
+NAME = "test";
+
 

d39433940e3ca6040989d02ece63cab04b77c9b7
Anders Bakken <agbakken@gmail.com>
2013-02-05 00:53:31 -0800

    initial commit

---
+++ j.js
@@ -0,0 +1,10 @@
+// j.js
+
+
+var target = {
+    name:"test",
+    sources: ['main.cpp', 'foo.cpp', {source:test.cpp, CXXFLAGS:"-g $CXXFLAGS"}, {source:utenstandardflags.cpp, CXXFLAGS:"-O3"} ],
+    CXXFLAGS: "-O2",
+    type:"library"
+}
+
